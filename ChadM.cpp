2aa75fb1d7ce665593d0a9149c6d1d5f9de583aa
Student <student@lab-311-022.cs.csubak.edu>
2019-02-14 12:00:05 -0800

    Added more code

--- ChadM.cpp
+++ ChadM.cpp
@@ -1,7 +1,20 @@
-void showChadPicture (int x , int y, GLuint textid) {
-	glColor4ub(255, 255, 255);
-	float fx = (float)x;
-	float fy = (float)y;
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "fonts.h"
+#include <string.h>
+
+void displayChad (float x , float y, GLuint textid) {
+    	float wid = 100.0;
+
 	glPushMatrix();
-	glTranslate
+	glTranslatef(x, y, 0.0);
+	glBindTexture(GL_TEXTURE_2D, textid);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid, -wid);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i(wid, wid);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i(wid, -wid);
+	glEnd();
+	glPopMatrix();
 }
