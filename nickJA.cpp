fc1637a5e476db314c34d088eccb0e45a34bfac6
Student <student@lab-311-023.cs.csubak.edu>
2019-02-14 11:03:50 -0800

    Added file & image

---
+++ nickJA.cpp
@@ -0,0 +1,21 @@
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "fonts.h"
+#include <string.h>
+
+
+void displayNick(float x, float y, Gluint texture)
+{
+	glPushMatrix();	
+	glTranslatef(x, y, 0.0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
+ 	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
+ 	glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
+ 	glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
+	glEnd();
+	glPopMatrix();
+
+}
