038bd0f2d1f3549a6974b66f0152eb91aba8894e
Nicholas Jackson <njackson@odin.cs.csubak.edu>
2019-02-15 11:28:36 -0800

    Added text to my file

--- nickJA.cpp
+++ nickJA.cpp
@@ -8,9 +8,10 @@
 void displayNick(float x, float y, GLuint texture)
 {
 	float wid = 100.0f;
-	glPushMatrix();	
+	glPushMatrix();
 	glTranslatef(x, y, 0.0);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	glColor3ub(255, 255, 255);
+    glBindTexture(GL_TEXTURE_2D, texture);
 	glBegin(GL_QUADS);
 	glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
  	glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
@@ -18,5 +19,16 @@ void displayNick(float x, float y, GLuint texture)
  	glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
 	glEnd();
 	glPopMatrix();
+
+    unsigned int color = 0x003366ff;
+    Rect r;
+    r.centerx = x;
+    r.centery = y + 120;
+    r.height = 100;
+    r.width = 500;
+    r.bot = r.centery - 15;
+    r.left = r.centerx;
+    ggprint16(&r, 16, color, "Nick Jackson");
+
 
 }
