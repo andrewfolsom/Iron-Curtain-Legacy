eb5e7723961550c8e62a0b5badc3779a2bde0ee7
Spencer Austin <saustin@odin.cs.csubak.edu>
2019-02-17 21:10:58 -0800

    fix styling

--- spencerA.cpp
+++ spencerA.cpp
@@ -5,19 +5,18 @@
 void displaySpencer(float x, float y, GLuint texture)
 {
     float wid = 100.0f;
+    unsigned int color = 0x00bfb418;
     glPushMatrix();
     glTranslatef(x, y, 0.0);
     glColor3ub(255, 255, 255);
     glBindTexture(GL_TEXTURE_2D, texture);
     glBegin(GL_QUADS);
-        glTexCoord2f(1.0f, 1.0f); glVertex2i(-wid,-wid);
-        glTexCoord2f(1.0f, 0.0f); glVertex2i(-wid, wid);
-        glTexCoord2f(0.0f, 0.0f); glVertex2i(wid, wid);
-        glTexCoord2f(0.0f, 1.0f); glVertex2i(wid, -wid);
+    glTexCoord2f(1.0f, 1.0f); glVertex2i(-wid,-wid);
+    glTexCoord2f(1.0f, 0.0f); glVertex2i(-wid, wid);
+    glTexCoord2f(0.0f, 0.0f); glVertex2i(wid, wid);
+    glTexCoord2f(0.0f, 1.0f); glVertex2i(wid, -wid);
     glEnd();
     glPopMatrix();
-
-    unsigned int color = 0x00bfb418;
     
     Rect r;
     r.width = 250.0;
