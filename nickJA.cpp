0cce2c28d8d2a938e81c3560425d89bb2f795460
Nick Jackson <njackson6@csub.edu>
2019-02-26 18:57:45 -0800

    added functionality

--- nickJA.cpp
+++ nickJA.cpp
@@ -13,6 +13,13 @@
 
 extern Game g;
 
+//const int RUSH = 0;
+//const int STRAFE = 1;
+//const int CIRCLING = 2;
+//const int BANK = 3;
+
+enum COLOR { RUSH, STRAFE, CIRCLING, BANK };
+
 //DISPLAY
 void displayNick(float x, float y, GLuint texture)
 {
@@ -50,10 +57,11 @@ void spawnOpFor(int x, int y, int movType)
 	newGuy->movPattern = movType;
 	g.numOpFor++;
 
-	printf("OpFor %i created at (%.2f, %.2f) with movePattern = %i\n",g.numOpFor, newGuy->pos[0], newGuy->pos[1], newGuy->movPattern);
+	printf( "OpFor %i created at (%.2f, %.2f) with movePattern = %i\n",
+			g.numOpFor, newGuy->pos[0], newGuy->pos[1], newGuy->movPattern);
 }
 
-//RENDEROPFOR
+//RENDER OPFOR
 void renderOpFor() 
 {
 
@@ -66,7 +74,7 @@ void renderOpFor()
 		glTranslatef(draw->pos[0], draw->pos[1], draw->pos[2]);
 		glBegin(GL_QUADS);
 		glVertex2f(-20.0, -20.0);
-		glVertex2f(-20.0,  20.0);
+		glVertex2f(-20.0, ` 20.0);
 		glVertex2f( 20.0,  20.0);
 		glVertex2f( 20.0, -20.0);
 		glEnd();
@@ -78,29 +86,62 @@ void renderOpFor()
 
 //1 - Rush
 //Enemy begins at the top of the screen and rushes straight to the bottom.
-void upDateRush() 
+//Iteration will be used to identify which entity to update the position of.
+void updateRush(int iteration)
 {
-
+	opForShip *move;
 }
 
 //2 - Strafing
 //Enemy Oscillates between to points while moving to the bottom of the screen.
-void upDateStrafe()
+//Iteration will be used to identify which entity to update the position of.
+void updateStrafe(int iteration)
 {
-
+	opForShip *move;
 }
 
 //3 - Circling
 //Enemy follows the path of a circle whose center is consistently moving
 //towards the bottom of the screen.
-void upDateCircle() 
+//Iteration will be used to identify which entity to update the position of.
+void updateCircle(int iteration) 
 {
-
+	opForShip *move;
 }
 
 //4 - Bank
 //Enemy follows a curve while moving from the top to bottom of the screen.
-void upDateBank()
+//Iteration will be used to identify which entity to update the position of.
+void updateBank(int iteration)
 {
+	opForShip *move;
+}
 
+//Movement Function
+//Function uses a switch case to determine what function should be applied to
+//each entity in the OpFor array.
+void updatePosition()
+{
+	for (int i = 0; i < g.numOpFor; i++) {
+		opForShip *target = &g.opFor[i];
+		
+		switch (target->movPattern) {
+
+			case (RUSH):
+				updateRush(target->movPattern);
+			break;
+
+			case (STRAFE):
+				updateStrafe(target->movPattern);
+			break;
+
+			case (CIRCLING):
+				updateCircle(target->movType);
+			break;
+
+			case (BANK):
+				updateBank(target->movType);
+			break;
+		}
+	}
 }
