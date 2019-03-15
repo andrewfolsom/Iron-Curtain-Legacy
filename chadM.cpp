76e8cd5fce0cd52369f9ce558227099cf26c49f4
Chad Manning <cmanning6@csub.edu>
2019-03-14 22:12:51 -0700

    Proper Ship destruction, go crazy.

--- chadM.cpp
+++ chadM.cpp
@@ -1,5 +1,12 @@
+/**
+ * Chad Manning's file for Iron Curtain
+ * @author: Chad Manning
+ * Course: CMPS 3350
+ * Instructor: Gordon Griesel 
+*/
 #include <GL/glx.h>
 #include <X11/Xlib.h>
+#include <stdio.h>
 #include "core.h"
 #include "chadM.h"
 #include "fonts.h"
@@ -8,6 +15,9 @@ extern Global& gl;
 extern EnemyShip *headShip;
 extern EnemyShip *tailShip;
 
+/*
+    Displays Chads picture and name on the title screen
+*/
 void displayChad (float x , float y, GLuint textid) {
     float wid = 100.0;
 
@@ -34,6 +44,15 @@ void displayChad (float x , float y, GLuint textid) {
     ggprint16(&r, 16, color, "Chad Manning");
 }
 
+/*
+    Constructor for EnemyShip class
+    If you want to make the ship behave differently call the config class
+        from nickJA.cpp to modify path.
+
+    Ex. 
+        eShip = new EnemyShip(x, y, movType);
+        tailShip->config$NAME_OF_PATTERN$;
+*/
 EnemyShip::EnemyShip(int x, int y, int movType)
 {
     pos[0] = x;
@@ -41,6 +60,7 @@ EnemyShip::EnemyShip(int x, int y, int movType)
     pos[2] = 1;
 	color[0] = color[1] = color[2] = 0.35;
     health = 100;
+    movPattern = movType;
     if (headShip == NULL) {
         headShip = tailShip = this;
         prevShip = nextShip = NULL;
@@ -51,16 +71,44 @@ EnemyShip::EnemyShip(int x, int y, int movType)
     nextShip = NULL;
 }
 
+/*
+    Destructor for EnemyShip class
+    Readjusts linked list depending on which ship was destroyed.
+*/
 EnemyShip::~EnemyShip()
 {
     if (prevShip != NULL) {
         prevShip->nextShip = nextShip;
+    } else {
+        headShip = nextShip;
     }
     if (nextShip != NULL) {
         nextShip->prevShip = prevShip;
+    } else {
+        tailShip = prevShip;
     }
 }
 
+/*
+    Returns radius of bounding circle for the ship
+*/
+float EnemyShip::getRadius() {
+    return detRadius;
+}
+
+/*
+    Destroys the ship when its health reaches zero. KABOOM.
+    "But Chad all this does is call the destructor." Yeah but destroyShip
+        makes more sense than delete.
+*/
+void EnemyShip::destroyShip() {
+    delete this;
+}
+
+/*
+    Takes a Ship class as input and renders it to screen.
+        used for both player ship and all enemy ships.
+*/
 void renderShip(Ship ship)
 {
 	glColor3fv(ship.color);
--- chadM.h
+++ chadM.h
@@ -1,10 +1,16 @@
+/**
+ * Header for all of Chad's functions. Implementation is in chadM.cpp
+ * The functions under By Jackson are located in nickJA.cpp since he wrote
+ * 	them all and deserves credit
+*/
 class EnemyShip : public Ship
 {
 	public :
 		EnemyShip(int x, int y, int movType);
 		~EnemyShip();
-
-		//EDITS by JACKSON
+		float getRadius();
+		void destroyShip();
+		//By JACKSON
 		void updateStrafe();
 		void configStrafe(float radius, float angleSet, float angleSpeed, float speed, int dir);
 		void updateRush();
@@ -17,13 +23,24 @@ class EnemyShip : public Ship
 		void configDiagRush(float x, float y, float speed);
 		void updatePosition();
 		//
-
-
 		EnemyShip *nextShip;
 		EnemyShip *prevShip;
 	private :
 		int health;
-		int numberOfEnemies;
+		// By Jackson
+		enum MOVETYPE { RUSH, STRAFE, CIRCLING, BANK, DIAG_RUSH };
+		Vec spawnPos;
+		float angle;
+		float angleSpd;
+		float t;
+		float destX, destY;
+		int speedMul = 1;
+		int initFlag = 0;
+		int moveFlag = -1;
+		int movPattern = RUSH;
+		float radius = 25.0;
+		int destOffset = 700;
+		//
 };
 
 class Grunt : protected EnemyShip
